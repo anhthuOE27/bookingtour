afa5508b1ecb73939fbfdae379b0573a7c469eb5
tantn-0943 <tran.ngoc.tan@sun-asterisk.com>
2019-09-26 10:27:15 +0700

    Destroy Booking

--- app/models/booking_tour.rb
+++ app/models/booking_tour.rb
@@ -1,4 +1,7 @@
 class BookingTour < ApplicationRecord
   belongs_to :user
   belongs_to :tour
+  enum status: {start: 0, started: 1}
+  delegate :name, to: :tour, prefix: :tour
+  scope :sort_by_created_at, ->{order created_at: :desc}
 end
